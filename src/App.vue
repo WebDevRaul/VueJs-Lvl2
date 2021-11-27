8e47ffcc1e46d7e0a74d7b04605ce4ca9ac6e2f2
TextError <savinraul1983@yahoo.com>
2021-11-27 18:09:23 +0000

    Animation

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,34 @@
   <keep-alive>
     <component :is="componentName"></component>
   </keep-alive>
+  <button type="button" @click="flag = !flag">Toggle</button>
+  <transition name='fade'>
+    <h1 v-show="flag">Hello world!</h1>
+  </transition>
+  <transition name='fade' mode="out-in">
+    <h1 v-if="flag" key="main">Hello world!</h1>
+    <h1 v-else key="secondary">Hello world 2!</h1>
+  </transition>
+
+  <button type="button" @click="flag = !flag">Animation</button>
+  <transition name='zoom' type='animation'>
+    <h1 v-if="flag">Hello world!</h1>
+  </transition>
+
+  <transition
+    @before-enter="beforeEnter"
+    @enter="enter"
+    @after-enter="afterEnter"
+    @before-leave="beforeLeave"
+    @leave="leave"
+    @after-leave="afterLeave"
+    @enter-cancel="enterCancel"
+    @leave-cancel="leaveCancel"
+    :css="false"
+  >
+    <h1 v-if="flag">Hello world!</h1>
+  </transition>
+
 </template>
 
 <script>
@@ -35,7 +63,8 @@ export default {
     return {
       age: 20,
       one: 'This is slot 1',
-      componentName: 'Home'
+      componentName: 'Home',
+      flag: false
     }
   },
   methods: {
@@ -44,7 +73,88 @@ export default {
     },
     onUpdateAgeCB(num) {
       this.age += num
+    },
+    beforeEnter(el, ) {
+      console.log('before-enter-hook', el)
+    },
+    enter(el, done) {
+      console.log('enter-hook', el);
+      const animation = el.animate(
+        [{transform: "scale3D(0,0,0)"},{}],
+        { duration: 1000 }  
+      );
+      animation.onfinish = () => done();
+    },
+    afterEnter(el) {
+      console.log('after-enter-hook', el)
+    },
+    beforeLeave(el) {
+      console.log('before-leave-hook', el)
+    },
+    leave(el, done) {
+      console.log('leave-hook', el)
+      const animation = el.animate(
+        [{}, {transform: "scale3D(0,0,0)"}],
+        { duration: 1000 }  
+      );
+      animation.onfinish = () => done();
+    },
+    afterLeave(el) {
+      console.log('after-leave-hook', el)
+    },
+    enterCancel(el) {
+      console.log('enter-cancel-hook', el)
+    },
+    leaveCancel(el) {
+      console.log('leave-cancel-hook', el)
     }
   }
 }
 </script>
+
+<style scoped>
+  .fade-enter-from {
+    opacity: 0;
+  }
+  .fade-enter-active {
+    transition: all .25s linear;
+  }
+  .fade-leave-to {
+    transition: all .25s linear;
+    opacity: 0;
+  }
+
+  .zoom-enter-active {
+    animation: zoom-in 1s linear forwards;
+    transition: all 2s linear;
+  }
+  .zoom-leave-active {
+    animation: zoom-out 1s linear forwards;
+    transition: all 2s linear;
+  }
+  .zoom-enter-from {
+    opacity: 0;
+  }
+  .zoom-leave-to {
+    opacity: 0;
+  }
+  
+  @keyframes zoom-in {
+    from {
+      transform: scale(0, 0);
+    }
+    to {
+      transform: scale(1, 1);
+    }
+  }
+
+  @keyframes zoom-out {
+    from {
+      transform: scale(1, 1);
+    }
+    to {
+      transform: scale(0, 0);
+    }
+  }
+
+</style>
