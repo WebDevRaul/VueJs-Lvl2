e67d5f2cce3b57293507aa77c36dd7647bddee02
TextError <savinraul1983@yahoo.com>
2021-11-27 17:03:22 +0000

    Dinamic components

--- src/App.vue
+++ src/App.vue
@@ -12,20 +12,30 @@
       <li>This is slot 3</li>
     </template>
   </Slot>
+  <select v-model="componentName">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+  <keep-alive>
+    <component :is="componentName"></component>
+  </keep-alive>
 </template>
 
 <script>
 import Title from './components/common/title/Title.vue';
 import Age from './components/app/age/Age.vue';
 import Slot from './components/common/slot/Slot.vue';
+import Home from './components/app/home/Home.vue';
+import About from './components/app/about/About.vue';
 
 export default {
   name: 'App',
-  components: { Title, Age, Slot },
+  components: { Title, Age, Slot, Home, About },
   data() {
     return {
       age: 20,
-      one: 'This is slot 1'
+      one: 'This is slot 1',
+      componentName: 'Home'
     }
   },
   methods: {
---
+++ src/components/app/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>
+    Home component
+  </h1>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+    activated() {
+      console.log('activated')
+    },
+    deactivated() {
+      console.log('deactivated')
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
