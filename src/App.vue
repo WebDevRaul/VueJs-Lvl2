41ced69b72e395ae9970999b3ce699ad2aed5d75
TextError <savinraul1983@yahoo.com>
2021-11-27 16:32:50 +0000

    Slots

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <Title title='Hello world!' />
   <Age :age="age" @onUpdateAge="onUpdateAge" :updateAgeFn='onUpdateAgeCB' />
+  <Slot>
+    <template v-slot:1>
+      <li>{{ one }}</li>
+    </template>
+    <template v-slot:2>
+      <li>This is slot 2</li>
+    </template>
+    <template v-slot:3>
+      <li>This is slot 3</li>
+    </template>
+  </Slot>
 </template>
 
 <script>
 import Title from './components/common/title/Title.vue';
 import Age from './components/app/age/Age.vue';
+import Slot from './components/common/slot/Slot.vue';
 
 export default {
   name: 'App',
-  components: { Title, Age },
+  components: { Title, Age, Slot },
   data() {
     return {
-      age: 20
+      age: 20,
+      one: 'This is slot 1'
     }
   },
   methods: {
---
+++ src/components/common/slot/Slot.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li>This is not a slot</li>
+    <slot name="1"></slot>
+    <li>This is not a slot</li>
+    <slot name="2"></slot>
+    <li>This is not a slot</li>
+    <slot name="3"></slot>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: "Slot"
+  }
+</script>
+
+<style scoped>
+
+</style>
